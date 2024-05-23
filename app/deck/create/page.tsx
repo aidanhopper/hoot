54a1482f91af474e8bd8d7da62ae952f0f45512a
Aidan Hopper <aidanhop1@gmail.com>
2024-05-22 21:27:48 -0700

    working on deck builder

--- app/deck/create/page.tsx
+++ app/deck/create/page.tsx
@@ -38,7 +38,51 @@ const CreateDeck = () => {
     );
   }
 
-
+  const Card = ({ className }: { className?: string }) => {
+    return (
+      <div className={`${className} mb-6`}>
+        <div className="w-full bg-white rounded-lg">
+          <div className="flex border-b-2 text-gray-400 p-4 font-bold text-lg">
+            <span className="flex-auto">
+              1
+            </span>
+            <button className="flex-auto text-right hover:text-gray-600 duration-300">
+              X
+            </button>
+          </div>
+          <div>
+            <div className="group flex w-full">
+              <div className="flex-auto font-bold content-center ml-3 text-gray-400">
+                Q
+              </div>
+              <input className="flex-auto text-center outline-none py-2 w-full 
+                font-bold w-3/4 border-white focus:border-black border-b-2" 
+                placeholder="Enter your question here"/>
+              <div className="flex-auto"/>
+            </div>
+            <div className="flex">
+              <div className="flex flex-auto flex-col">
+                <div className="flex-auto">
+                  aasdf
+                </div>
+                <div className="flex-auto">
+                  aasdf
+                </div>
+              </div>
+              <div className="flex flex-auto flex-col">
+                <div className="flex-auto">
+                  aasdf
+                </div>
+                <div className="flex-auto">
+                  aasdf
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="bg-gray-100 h-screen font-sans overflow-hidden">
@@ -48,7 +92,7 @@ const CreateDeck = () => {
         </div>
         <div className="flex-auto container m-auto pt-[50px] px-[200px]">
           <div className="flex">
-            <span className="flex-auto font-bold text-xl">
+            <span className="flex-auto font-bold text-xl content-center">
               Create a new deck
             </span>
             <span className="flex-auto text-right">
@@ -58,8 +102,40 @@ const CreateDeck = () => {
               </button>
             </span>
           </div>
-          <div className="flex-auto">
-            asdf
+          <div className="flex-auto mt-12">
+            <label className="group">
+              <span className="absolute text-gray-500 font-bold pt-1 text-xs px-4">
+                Title
+              </span>
+              <input className="font-bold outline-none pt-6 px-4 pb-1 w-full
+                border-b-2 border-white focus:border-black rounded-lg"
+                placeholder="Enter a title for your deck like best frog species for eating all the stupid bugs in your house that just dont go away even though you bought a bug zapper" />
+            </label>
+            <div className="flex mt-6">
+              <div className="flex-auto w-full">
+                <div className="pr-6">
+                  <span className="absolute text-xs text-gray-400 font-bold pt-1 px-4">
+                    Add a description...
+                  </span>
+                  <textarea className="w-full border-b-2 border-white focus:border-black
+                    resize-none outline-none rounded-lg pt-6 px-4 h-32"
+                    rows={1} cols={5} />
+                </div>
+              </div>
+              <div className="flex-auto w-full">
+                <div className="pl-6">
+                  placeholder
+                </div>
+              </div>
+            </div>
+            <button className="border-gray-400 hover:bg-gray-200 text-blue-800
+              border-2 duration-200 py-1 px-4 rounded-lg my-12">
+              Import
+            </button>
+            <div className="flex-auto">
+              <Card/>
+              <Card/>
+            </div>
           </div>
         </div>
       </div>
